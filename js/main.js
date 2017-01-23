065312d1e719c48b73be32020e3ae0d2f1c981d2
Tomas Pelak <s3w3nofficial@gmail.com>
2017-01-23 20:07:53 +0100

    js changed

---
+++ js/main.js
@@ -0,0 +1,65 @@
+ $(document).ready(function (){
+     $("#But0").click(function (){
+         $('html, body').animate({
+             scrollTop: $("#carousel").offset().top
+         }, 1000);
+         return false;
+     });
+ });
+
+ $(document).ready(function (){
+     $("#But1").click(function (){
+         $('html, body').animate({
+             scrollTop: $("#novinky").offset().top-80
+         }, 1000);
+         return false;
+     });
+ });
+
+ $(document).ready(function (){
+    $("#But2").click(function (){
+        $('html, body').animate({
+             scrollTop: $("#onas").offset().top-80
+         }, 1000);
+         return false;
+     });
+ });
+ $(document).ready(function (){
+     $("#But3").click(function (){
+         $('html, body').animate({
+             scrollTop: $("#clenove").offset().top-80
+         }, 1000);
+         return false;
+     });
+ });
+
+ $(document).ready(function (){
+    $("#But4").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#servery").offset().top-80
+        }, 1000);
+        return false;
+     });
+ });
+
+$(document).ready(function (){
+    $("#But5").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#galerie").offset().top-80
+        }, 1000);
+        return false;
+     });
+ });
+
+ $(document).ready(function () {
+     $('.nav a').click(function(e) {
+
+         $('.nav a').removeClass("aktivni");
+
+         var $this = $(this);
+         if (!$this.hasClass('aktivni')) {
+             $this.addClass('aktivni');
+         }
+         //e.preventDefault();
+     });
+ });
