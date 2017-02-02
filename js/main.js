135b6c14959824c20c8a9c21c16c0856f9038e48
root <tomik573seznam.cz>
2017-02-02 22:03:24 +0100

    editted freeform

--- js/main.js
+++ js/main.js
@@ -74,7 +74,6 @@ $(document).ready(function (){
          //e.preventDefault();
      });
  });
-
 $(function() { 
     Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.4.5/themes/classic/galleria.classic.min.js');
     Galleria.run('.galleria');
